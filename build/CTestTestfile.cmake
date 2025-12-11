c4c3bd302523da50fc3cf31070069a9d2fc9d231
soma <1720771239@qq.com>
2025-12-11 19:32:45 +0800

    Stop tracking build and third_party directories

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/soma/code/trae/12-02/02/06
-# Build directory: /Users/soma/code/trae/12-02/02/06/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
