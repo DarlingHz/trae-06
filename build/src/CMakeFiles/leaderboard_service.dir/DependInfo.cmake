6993af568d94196a30cd00806587e40cc86b4188
soma <1720771239@qq.com>
2025-12-11 19:03:19 +0800

    整理仓库

---
+++ build/src/CMakeFiles/leaderboard_service.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/soma/code/trae/12-01-10/06/src/controller/GameController.cpp" "src/CMakeFiles/leaderboard_service.dir/controller/GameController.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/controller/GameController.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/controller/LeaderboardController.cpp" "src/CMakeFiles/leaderboard_service.dir/controller/LeaderboardController.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/controller/LeaderboardController.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/controller/ScoreController.cpp" "src/CMakeFiles/leaderboard_service.dir/controller/ScoreController.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/controller/ScoreController.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/controller/UserController.cpp" "src/CMakeFiles/leaderboard_service.dir/controller/UserController.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/controller/UserController.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/repository/SQLiteGameRepository.cpp" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteGameRepository.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteGameRepository.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/repository/SQLiteLeaderboardRepository.cpp" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteLeaderboardRepository.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteLeaderboardRepository.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/repository/SQLiteScoreRepository.cpp" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteScoreRepository.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteScoreRepository.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/repository/SQLiteSessionRepository.cpp" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteSessionRepository.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteSessionRepository.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/repository/SQLiteUserRepository.cpp" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteUserRepository.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/repository/SQLiteUserRepository.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/service/GameService.cpp" "src/CMakeFiles/leaderboard_service.dir/service/GameService.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/service/GameService.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/service/LeaderboardService.cpp" "src/CMakeFiles/leaderboard_service.dir/service/LeaderboardService.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/service/LeaderboardService.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/service/ScoreService.cpp" "src/CMakeFiles/leaderboard_service.dir/service/ScoreService.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/service/ScoreService.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/service/SessionService.cpp" "src/CMakeFiles/leaderboard_service.dir/service/SessionService.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/service/SessionService.cpp.o.d"
+  "/Users/soma/code/trae/12-01-10/06/src/service/UserService.cpp" "src/CMakeFiles/leaderboard_service.dir/service/UserService.cpp.o" "gcc" "src/CMakeFiles/leaderboard_service.dir/service/UserService.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/leaderboard_service.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leaderboard_service.dir/controller/GameController.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/controller/GameController.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/controller/LeaderboardController.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/controller/LeaderboardController.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/controller/ScoreController.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/controller/ScoreController.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/controller/UserController.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/controller/UserController.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteGameRepository.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteGameRepository.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteLeaderboardRepository.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteLeaderboardRepository.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteScoreRepository.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteScoreRepository.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteSessionRepository.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteSessionRepository.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteUserRepository.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/repository/SQLiteUserRepository.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/service/GameService.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/service/GameService.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/service/LeaderboardService.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/service/LeaderboardService.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/service/ScoreService.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/service/ScoreService.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/service/SessionService.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/service/SessionService.cpp.o.d"
+  "CMakeFiles/leaderboard_service.dir/service/UserService.cpp.o"
+  "CMakeFiles/leaderboard_service.dir/service/UserService.cpp.o.d"
+  "libleaderboard_service.a"
+  "libleaderboard_service.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leaderboard_service.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
